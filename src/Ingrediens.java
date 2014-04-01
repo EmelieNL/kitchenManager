7bde9c0ed1baa920c5e7652601cc94a18c5e570b
emelienl <emelie.nl@gmail.com>
2014-04-01 12:37:26 +0200

    Menyalt. 5 funkar nästan (går att skriva in ett felaktigt receptnamn)

--- src/Ingrediens.java
+++ src/Ingrediens.java
@@ -1,4 +1,19 @@
-
 public class Ingrediens {
+
+	int amount;
+	String name;
+
+	public Ingrediens(String name, int amount) {
+		this.name = name;
+		this.amount = amount;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public String getName() {
+		return name;
+	}
 
 }
--- src/Menu.java
+++ src/Menu.java
@@ -4,6 +4,8 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Types;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Menu {
@@ -13,12 +15,20 @@ public class Menu {
 	Statement stmt;
 	ResultSet res;
 	Connection conn;
+	ResultSetMetaData rsmd;
+
+	ArrayList<Ingrediens> inKitchen;
+	ArrayList<Ingrediens> inRecipe;
+	ArrayList<Ingrediens> shoppingList;
 
 	public Menu(Database connection) throws SQLException {
 		this.connection = connection;
 		this.sc = new Scanner(System.in);
 		stmt = connection.getStatement();
 		conn = connection.getConnection();
+		shoppingList = new ArrayList<Ingrediens>();
+		inKitchen = new ArrayList<Ingrediens>();
+		inRecipe = new ArrayList<Ingrediens>();
 		printWelcomeMessage();
 	}
 
@@ -49,6 +59,9 @@ public class Menu {
 		executeCommand(choice);
 	}
 
+	/*
+	 * Loopar menyn
+	 */
 	private void loopMenu() throws SQLException {
 		System.out.println("");
 		System.out.println("****** ****** \n");
@@ -56,6 +69,9 @@ public class Menu {
 		printMenu();
 	}
 
+	/*
+	 * Utför olika åtgärder beroende på vilket val användaren gör
+	 */
 	private void executeCommand(int choice) throws SQLException {
 		switch (choice) {
 			case 1:
@@ -100,8 +116,11 @@ public class Menu {
 				choice = 5;
 				System.out.println("Enter recipes name \n");
 				String recipeNameUpd = getCommandString();
+				shoppingList.clear();
+				inKitchen.clear();
+				inRecipe.clear();
 				if (possibleRecipe(recipeNameUpd)) {
-					updateKitchen(recipeNameUpd);
+					cookRecipe(recipeNameUpd);
 					System.out.println("Kitchen storage updated! \n");
 				} else
 					System.out.println("Not enough ingredients");
@@ -116,18 +135,13 @@ public class Menu {
 		}
 	}
 
-	private boolean possibleRecipe(String recipeNameUpd) {
-		String query = "SELECT * FROM inrecipe A JOIN inkitchen" + " B ON A.ingrediensname = B.name WHERE A.recipename = '" + recipeNameUpd + "'";
-		return true;
-	}
-
 	/*
-	 * Case 5:
+	 * Case 1: Listar alla befintliga ingredienser i köket Metoden skriver ut
+	 * ingrediensens namn och amount till konsoll
 	 */
-	private boolean updateKitchen(String recipeNameUpd) {
-		String query = "SELECT * FROM recipes";
+	private boolean listAllIngKitchen() {
+		String query = "SELECT * FROM inkitchen";
 		return exQuery(query);
-
 	}
 
 	/*
@@ -139,26 +153,7 @@ public class Menu {
 	}
 
 	/*
-	 * Case 1: Listar alla befintliga ingredienser i köket Metoden skriver ut
-	 * ingrediensens namn och amount till konsoll
-	 */
-	private boolean listAllIngKitchen() {
-		String query = "SELECT * FROM inkitchen";
-		return exQuery(query);
-	}
-
-	/*
-	 * Case 4: Listar alla befintliga ingredienser i ett recept (från input)
-	 * Metoden skriver ut ingrediensens namn och amount till konsoll
-	 */
-	private boolean listAllIngRec(String recipeName) {
-		String query = "SELECT ingrediensname, amount FROM inrecipe WHERE recipename = '" + recipeName + "'";
-		return exQuery(query);
-
-	}
-
-	/*
-	 * Case 3 Lägg till ingrediens till kök Kollar om ingrediensen redan
+	 * Case 3: Lägg till ingrediens till kök Kollar om ingrediensen redan
 	 * existerar och i sådana fall uppdaterar med rätt amount Annars lägger till
 	 * ingrediensen
 	 */
@@ -170,6 +165,134 @@ public class Menu {
 			String query = "INSERT INTO inkitchen (name, amount) VALUES ('" + ingredientName + "', '" + ingredientAmount + "')";
 			return exQueryUpd(query);
 		}
+	}
+
+	/*
+	 * Kollar om en ingrediens existerar i köket baserat på inkommande
+	 * ingrediensnamn
+	 */
+	private boolean checkIfIngExists(String ingName) throws SQLException {
+		String query = "SELECT * FROM inkitchen WHERE name = '" + ingName + "'";
+		res = stmt.executeQuery(query);
+		if (res.isBeforeFirst()) { // rader finns i resultSet
+			return true;
+		}
+		return false;
+	}
+
+	/*
+	 * Case 4: Listar alla befintliga ingredienser i ett recept (från input)
+	 * Metoden skriver ut ingrediensens namn och amount till konsoll
+	 */
+	private boolean listAllIngRec(String recipeName) {
+		String query = "SELECT ingrediensname, amount FROM inrecipe WHERE recipename = '" + recipeName + "'";
+		return exQuery(query);
+	}
+
+	/*
+	 * Case 5: kollar om tillräckligt med ingredienser finns i köket
+	 */
+	private boolean possibleRecipe(String recipeNameUpd) throws SQLException {
+		if (ifIngExists(recipeNameUpd)) {
+			return true;
+		}
+		return false;
+	}
+
+	/*
+	 * check if all recipe ingredients exists in kitchen
+	 */
+	private boolean ifIngExists(String recipeName) throws SQLException {
+		String query = "SELECT ingrediensname, amount FROM inrecipe WHERE recipename = '" + recipeName + "'";
+		res = stmt.executeQuery(query); // alla ingredienser i ett recept
+		addAll(res, inRecipe);
+
+		// if (res.wasNull())
+		// return true;
+		// res.close();
+
+		query = "SELECT name, amount FROM inkitchen";
+		res = stmt.executeQuery(query); // alla ingredienser i köket
+		addAll(res, inKitchen);
+
+		// loopar genom alla ingredienser som receptet behöver samt kontrollerar
+		// mängden
+		Iterator<Ingrediens> it = inRecipe.iterator();
+		while (it.hasNext()) {
+			Ingrediens tempIng = it.next();
+			if (!checkIngByName(tempIng, inKitchen) || !checkIngByAmount(tempIng, inKitchen))
+				return false;
+		}
+		return true;
+	}
+
+	/*
+	 * Skapar en ny ingredienslista från inkommande resultSet
+	 */
+	private void addAll(ResultSet resSet, ArrayList<Ingrediens> list) throws SQLException {
+		while (resSet.next()) {
+			String ingName = resSet.getString(1);
+			int ingAmount = resSet.getInt(2);
+			list.add(new Ingrediens(ingName, ingAmount));
+		}
+	}
+
+	/*
+	 * Slår upp ingrediensnamnet i köket
+	 */
+	private boolean checkIngByName(Ingrediens recIng, ArrayList<Ingrediens> kitchenList) {
+
+		String recIngNamn = recIng.getName();
+		int kitchenListSize = kitchenList.size();
+		for (int n = 0; n < kitchenListSize; n++) {
+			if (recIngNamn.equals(kitchenList.get(n).getName())) { // vi har en
+				// match
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/*
+	 * Slår upp amount i köket
+	 */
+	private boolean checkIngByAmount(Ingrediens recIng, ArrayList<Ingrediens> kitchenList) {
+		int recIngAmount = recIng.getAmount();
+		String recIngName = recIng.getName();
+		int kitchenListSize = kitchenList.size();
+
+		for (int n = 0; n < kitchenListSize; n++) {
+			int amountDiff = (kitchenList.get(n).getAmount() - recIngAmount);
+			if (amountDiff >= 0) { // vi har tillräckligt
+				shoppingList.add(new Ingrediens(recIngName, amountDiff));
+				return true;
+			} else {
+				shoppingList.add(new Ingrediens(recIngName, amountDiff));
+			}
+		}
+		return false;
+	}
+
+	/*
+	 * Case 5: "Lagar" ett recept om tillräckligt med ingredienser finns
+	 */
+	private boolean cookRecipe(String recipeNameUpd) throws SQLException {
+		int size = shoppingList.size();
+		String query = ("SELECT * FROM inkitchen");
+		res = stmt.executeQuery(query);
+
+		for (int i = 0; i < size; i++) {
+			Ingrediens tempIng = shoppingList.get(i);
+			while (res.next()) {
+				if (res.getString(1).equals(tempIng.getName())) {
+					String queryUpd = "UPDATE inkitchen SET amount = '" + tempIng.getAmount() + "' WHERE name = '" + tempIng.getName() + "'";
+					exQueryUpd(queryUpd);
+					break;
+				}
+				res.beforeFirst();
+			}
+		}
+		return true;
 	}
 
 	/*
@@ -189,43 +312,10 @@ public class Menu {
 	}
 
 	/*
-	 * Exekverar en query och skapar ett resultatobjekt för utskrift Skriver ut
-	 * resultatet på konsoll
-	 */
-	private boolean exQuery(String query) {
-		try {
-			res = stmt.executeQuery(query);
-			printResult(res);
-			return true;
-		} catch (SQLException e) {
-			System.err.println(e.getLocalizedMessage());
-			e.printStackTrace();
-			return false;
-		}
-
-	}
-
-	/*
-	 * Exekverar en query för uppdatering av en tabell-rad
-	 */
-	private boolean exQueryUpd(String query) {
-		try {
-			stmt = conn.createStatement();
-			stmt.executeUpdate(query);
-			return true;
-		} catch (SQLException e) {
-			System.err.println(e.getLocalizedMessage());
-			e.printStackTrace();
-			return false;
-		}
-
-	}
-
-	/*
 	 * Tar in ett ResultSet och skriver ut varje resultat till konsoll
 	 */
 	private void printResult(ResultSet res) throws SQLException {
-		ResultSetMetaData rsmd = res.getMetaData();
+		rsmd = res.getMetaData();
 		int columnsNumber = rsmd.getColumnCount();
 		while (res.next()) {
 			for (int i = 1; i < columnsNumber + 1; i++) {
@@ -247,18 +337,36 @@ public class Menu {
 			return false;
 		}
 		return true;
-
 	}
 
 	/*
-	 * Kollar om en ingrediens existerar i köket
+	 * Exekverar en query och skapar ett resultatobjekt för utskrift Skriver ut
+	 * resultatet på konsoll
 	 */
-	private boolean checkIfIngExists(String ingName) throws SQLException {
-		String query = "SELECT * FROM inkitchen WHERE name = '" + ingName + "'";
-		res = stmt.executeQuery(query);
-		if (res.isBeforeFirst()) { // rader finns i resultSet
+	private boolean exQuery(String query) {
+		try {
+			res = stmt.executeQuery(query);
+			printResult(res);
 			return true;
+		} catch (SQLException e) {
+			System.err.println(e.getLocalizedMessage());
+			e.printStackTrace();
+			return false;
 		}
-		return false;
+	}
+
+	/*
+	 * Exekverar en query för uppdatering av en tabell-rad
+	 */
+	private boolean exQueryUpd(String query) {
+		try {
+			stmt = conn.createStatement();
+			stmt.executeUpdate(query);
+			return true;
+		} catch (SQLException e) {
+			System.err.println(e.getLocalizedMessage());
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
